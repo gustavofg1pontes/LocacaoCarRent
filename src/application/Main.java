4e1be1490a8aef04e03ff85b983ebf5ae6f77e37
Gustavo <gpontesf06@gmail.com>
2023-02-19 01:04:50 -0300

    First Commit starting a solution without interfaces to a java's course problem

---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        System.out.println("Entre com os dados do aluguel");
+        System.out.println("Modelo do carro: ");
+        String carModel = sc.nextLine();
+        System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+        CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+        sc.close();
+    }
+}
